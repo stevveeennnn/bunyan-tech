f425504bfa9644fbe1977ca93c26297a1890ae3e
steven <steven@localhost.localdomain>
2025-06-30 17:03:29 +0300

    initial push after repo reset

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/approved" element={<ApprovedSection/>} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
